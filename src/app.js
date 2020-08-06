1c1f988a478a5ff6bab735cdbbe2ba143321ef92
Tat Leung <tat.leung@piercewashington.com>
2020-08-05 22:57:06 -0700

    change port to 3300; stringify json)

--- src/app.js
+++ src/app.js
@@ -4,14 +4,14 @@ const axios = require("axios");
 const app = express();
 app.use(express.json());
 
-const port = 3000;
+const port = 3300;
 
 app.all("/proxy", async (req, res) => {
   let status = 400;
   let respData = {};
 
   console.log("\n\n>>> request:\n");
-  console.log(req.body);
+  console.log(JSON.stringify(req.body, null, 2));
   const response = await axios({
     method: req.query.method,
     url: req.query.targetUrl,
@@ -22,7 +22,7 @@ app.all("/proxy", async (req, res) => {
   respData = response.data;
 
   console.log("\n\n<<< response:\n");
-  console.log(respData);
+  console.log(JSON.stringify(respData, null, 2));
   res.status(status).send(respData);
 });
 
